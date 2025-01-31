cf2eb2654ef219cdd82f09b85a828feac3b10f08
VadimKorobec <_s_e_t__@ukr.net>
2025-01-31 14:26:07 +0200

    add some code

--- screens/StartGameScreen.jsx
+++ screens/StartGameScreen.jsx
@@ -1,10 +1,25 @@
-import { StyleSheet, TextInput, View } from "react-native";
+import { Alert, StyleSheet, TextInput, View } from "react-native";
 import PrimaryButton from "../components/PrimaryButton";
+import { useState } from "react";
 
 const StartGameScreen = () => {
+  const [enteredNumber, setEnteredNumber] = useState(0);
+
+  const handleInput = (number) => {
+    if (number < 1 || number > 99) {
+      return Alert.alert("Please enter number from 1 to 99");
+    }
+    setEnteredNumber(number);
+  };
+
   return (
     <View style={styles.inputContainer}>
-      <TextInput />
+      <TextInput
+        style={styles.numberInput}
+        value={enteredNumber}
+        onChangeText={handleInput}
+        keyboardType="numeric"
+      />
       <PrimaryButton>Reset</PrimaryButton>
       <PrimaryButton>Confirm</PrimaryButton>
     </View>
@@ -15,6 +30,26 @@ export default StartGameScreen;
 
 const styles = StyleSheet.create({
   inputContainer: {
-    flex: 1,
+    marginTop: 100,
+    marginHorizontal: 24,
+    padding: 16,
+    backgroundColor: "#72063c",
+    borderRadius: 8,
+    elevation: 4,
+    shadowColor: "black",
+    shadowOffset: { width: 0, height: 2 },
+    shadowRadius: 6,
+    shadowOpacity: 0.25,
+  },
+  numberInput: {
+    height: 50,
+    width: 50,
+    fontSize: 32,
+    borderBottomColor: "#ddb52f",
+    borderBottomWidth: 2,
+    color: "#ddb52f",
+    marginVertical: 8,
+    fontWeight: "bold",
+    textAlign: "center",
   },
 });
